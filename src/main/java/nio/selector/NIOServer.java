fab6dc32191e4f61fce7f2412ec406ca139fe9d0
programmer-liao <2380167274@qq.com>
2022-12-29 09:45:50 +0800

    feat: NIOServer案例演示

---
+++ src/main/java/nio/selector/NIOServer.java
@@ -0,0 +1,47 @@
+package nio.selector;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author liaodezhi
+ * @date 2022/12/29
+ * NIOServer案例演示
+ */
+public class NIOServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        Selector selector = Selector.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (true) {
+            if (selector.select(1000) == 0) {
+                System.out.println("服务器等待了一秒，无连接");
+                continue;
+            }
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                if (key.isAcceptable()) {
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    System.out.println("客户端连接成功，生成了一个socketChannel：" + socketChannel.hashCode());
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                }
+                if (key.isReadable()) {
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    channel.read(buffer);
+                    System.out.println("from 客户端：" + new String(buffer.array()));
+                }
+                keyIterator.remove();
+            }
+        }
+    }
+}
