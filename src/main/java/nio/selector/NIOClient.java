d60c7fb8236eb80f15a9aab85b429a8e564e8868
programmer-liao <2380167274@qq.com>
2022-12-29 09:46:03 +0800

    feat: NIOClient案例演示

---
+++ src/main/java/nio/selector/NIOClient.java
@@ -0,0 +1,28 @@
+package nio.selector;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author liaodezhi
+ * @date 2022/12/29
+ * NIOClient案例演示
+ */
+public class NIOClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 6666);
+        if (!socketChannel.connect(inetSocketAddress)) {
+            while (!socketChannel.finishConnect()) {
+                System.out.println("因为连接需要时间，客户端不会阻塞，可以做其他工作");
+            }
+        }
+        String str = "Hello, World";
+        ByteBuffer byteBuffer = ByteBuffer.wrap(str.getBytes());
+        socketChannel.write(byteBuffer);
+        System.in.read();
+    }
+}
